7cdc347ed30ea6037b0450d3d11c558b36ed0461
Cesare Bek <cesarebek1@gmail.com>
2020-11-09 21:36:35 +0100

    Added the FAQ section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Question <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            praesentium nam delectus nemo. Sequi, explicabo.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            praesentium nam delectus nemo. Sequi, explicabo.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different Payment Method</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            praesentium nam delectus nemo. Sequi, explicabo.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What products do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            praesentium nam delectus nemo. Sequi, explicabo.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
