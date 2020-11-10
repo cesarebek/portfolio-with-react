da7db16806bc778a83fae58f8921645ee8bf0b6f
Cesare Bek <cesarebek1@gmail.com>
2020-11-10 11:41:19 +0100

    Setup of styled components and basic styling to components and global styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 //I've created a folder pages that contains all the components of the current page.
 //Every time I'll make significal changes I'll commit this project.
 import React from 'react';
+//Global Style
+import GlobalStyle from './components/GlobaLStyle';
+//Import Pages
 import AboutMe from './Pages/AboutMe';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutMe />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,36 +1,68 @@
 import React from 'react';
 //This if I want to import an image from another folder
 import home1 from '../img/home1.png';
+import styled from 'styled-components';
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <StyledAbout>
+      <StyledDescription>
         <div className="title">
           {/* I'm adding this HIDE div because I want to reveal the text */}
-          <div className="hide">
+          <StyledHide>
             <h1>I work to develop</h1>
-          </div>
-          <div className="hide">
+          </StyledHide>
+          <StyledHide>
             <h1>
               your <span>needs</span>
             </h1>
-          </div>
-          <div className="hide">
+          </StyledHide>
+          <StyledHide>
             <h1>come true.</h1>
-          </div>
+          </StyledHide>
         </div>
         <p>
           Contact me for any front and back end project, I'll be glad to develop
           all your needs for your business.
         </p>
         <button>Contact Me</button>
-      </div>
-      <div className="image">
+      </StyledDescription>
+      <StyledImage>
         <img src={home1} alt="guy with camera" />
-      </div>
-    </div>
+      </StyledImage>
+    </StyledAbout>
   );
 };
 
+//Styled Components
+const StyledAbout = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const StyledDescription = styled.div`
+  flex: 1;
+  padding: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const StyledImage = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    /* This trick allows me to zoom in into the image without distort it with the width of 100% */
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+const StyledHide = styled.div`
+  overflow: hidden;
+`;
 export default AboutSection;
