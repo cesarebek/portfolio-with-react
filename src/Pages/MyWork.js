3e988d6006f9f541fb89ea9348d4dcae3cbda9fa
Cesare Bek <cesarebek1@gmail.com>
2020-11-10 15:55:19 +0100

    Creation of MyWork Section

--- src/Pages/MyWork.js
+++ src/Pages/MyWork.js
@@ -1,17 +1,59 @@
 import React from 'react';
 //import images
 import athlete from '../img/athlete-small.png';
-import athlete from '../img/theracer-small.png';
-import athlete from '../img/goodtimes-small.png';
+import theracer from '../img/theracer-small.png';
+import goodtimes from '../img/goodtimes-small.png';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const MyWork = () => {
   return (
-    <div>
-      <h1>My WORK</h1>
-    </div>
+    <StyledWork>
+      <StyledMovie>
+        <h2>The Athlete</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={athlete} alt="athlete" />
+        </Link>
+      </StyledMovie>
+      <StyledMovie>
+        <h2>The Racer</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={theracer} alt="racer" />
+        </Link>
+      </StyledMovie>
+      <StyledMovie>
+        <h2>Good Times</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={goodtimes} alt="goodtimes" />
+        </Link>
+      </StyledMovie>
+    </StyledWork>
   );
 };
+
+const StyledWork = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+  h2 {
+    padding: 1rem 0;
+  }
+`;
+const StyledMovie = styled.div`
+  padding-bottom: 10rem;
+  .line {
+    height: 0.5rem;
+    background: #cccccc;
+    margin-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
 
 export default MyWork;
